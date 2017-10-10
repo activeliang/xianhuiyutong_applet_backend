26e5d59a2a5d6a98fc08d8441d738d8a103c7695
activeliang <activeliang@gmail.com>
2017-10-10 23:45:03 +0800

    implement wechat_user required, add collection function" "

--- app/models/wechat_user.rb
+++ app/models/wechat_user.rb
@@ -1,4 +1,7 @@
 class WechatUser < ApplicationRecord
   belongs_to :user, optional: true
   has_many :login_logs, dependent: :destroy
+
+  validates_presence_of :client_token, message: "用户client_token不能为空！"
+
 end
