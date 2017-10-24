d2bbe58fe38f06eea5a776f469506725556f2ade
activeliang <activeliang@gmail.com>
2017-10-24 12:06:44 +0800

    fix product_new bug

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,14 +3,14 @@ sh "ssh-add"
 # config valid only for current version of Capistrano
 lock "3.9.1"
 
-set :application, "huiyu_wechat"
-set :repo_url, "git@github.com:activeliang/zhiai_jewelry_applet_rails_backend.git"
+set :application, "xian_huiyu"
+set :repo_url, "git@github.com:activeliang/xianhuiyutong_applet_backend.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deploy/huiyu_wechat"
+set :deploy_to, "/home/deploy/xian_huiyu"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
